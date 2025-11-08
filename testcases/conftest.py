0b1f48667e6685162cb0db7970dd4259618f7c7a
mohamedtaalat <mohamedabotalat57@gmail.com>
2025-11-08 16:41:17 +0200

    edit conftest

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -1,10 +1,18 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope="class")
 def driver(request):
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    service = Service()
+    driver = webdriver.Chrome(service=service, options=options)
+    request.cls.driver = driver
     driver.get("https://automationexercise.com")
     driver.maximize_window()
     yield driver
